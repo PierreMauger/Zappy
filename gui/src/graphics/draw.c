4a317928d6c417169fe6f57d8d642c53baa62529
PierreMauger <pierre.mauger@epitech.eu>
2022-06-20 21:54:01 +0200

    ADDED: empty texture test

--- gui/src/graphics/draw.c
+++ gui/src/graphics/draw.c
@@ -34,9 +34,22 @@ Texture *getTextures()
 
 void parse_map(client_t *client)
 {
+    int width = GetScreenWidth();
+    int height = GetScreenHeight();
+    int ratio_x = width / client->size_map.x;
+    int ratio_y = height / client->size_map.y;
+    int ratio = ratio_x < ratio_y ? ratio_x : ratio_y;
+
+    printf("%s%d %d%s\n", R, client->size_map.y, client->size_map.x, W);
     for (int i = 0; i < client->size_map.y; i++) {
         for (int j = 0; j < client->size_map.x; j++) {
-
+            DrawRectangle(i * ratio, j * ratio, ratio, ratio, RED);
+            DrawTexturePro(getTextures()[0],
+            (Rectangle){0, 0, 16, 16},
+            (Rectangle){i * ratio, j * ratio, ratio, ratio},
+            // (Rectangle){0, 0, 16, 16},
+            (Vector2){0, 0}, 0, WHITE);
+            // DrawTexture(getTextures()[0], i * ratio, j * ratio, WHITE);
         }
     }
 }
@@ -45,6 +58,7 @@ void draw_map(client_t *client)
 {
     BeginDrawing();
     ClearBackground(RAYWHITE);
+    DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
     parse_map(client);
     EndDrawing();
 }
