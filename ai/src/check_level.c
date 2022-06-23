d1c7a01ce55abc3e44c12bf988cae5256941f467
xavier <xavier.tonnellier@epitech.eu>
2022-06-23 16:50:01 +0200

    [ADD] complete check_level_function

--- ai/src/check_level.c
+++ ai/src/check_level.c
@@ -9,10 +9,16 @@
 
 bool check_level_1(client_t *client)
 {
+    if (client->player->inv->linemate < 1)
+        return false;
     return true;
 }
 
 bool check_level_2(client_t *client)
 {
+    inventory_t *inv = client->player->inv;
+
+    if (inv->linemate < 1 || inv->deraumere < 1 || inv->sibur < 1)
+        return false;
     return true;
 }
--- ai/src/check_level2.c
+++ ai/src/check_level2.c
@@ -9,25 +9,49 @@
 
 bool check_level_3(client_t *client)
 {
+    inventory_t *inv = client->player->inv;
+
+    if (inv->linemate < 2 || inv->sibur < 1 || inv->phiras < 2)
+        return false;
     return true;
 }
 
 bool check_level_4(client_t *client)
 {
+    inventory_t *inv = client->player->inv;
+
+    if (inv->linemate < 1 || inv->sibur < 2 || inv->phiras < 1
+        || inv->deraumere < 1)
+        return false;
     return true;
 }
 
 bool check_level_5(client_t *client)
 {
+    inventory_t *inv = client->player->inv;
+
+    if (inv->linemate < 1 || inv->sibur < 1 || inv->mendiane < 3
+        || inv->deraumere < 2)
+        return false;
     return true;
 }
 
 bool check_level_6(client_t *client)
 {
+    inventory_t *inv = client->player->inv;
+
+    if (inv->linemate < 1 || inv->deraumere < 2 || inv->sibur < 3
+        || inv->phiras < 1)
+        return false;
     return true;
 }
 
 bool check_level_7(client_t *client)
 {
+    inventory_t *inv = client->player->inv;
+
+    if (inv->linemate < 2 || inv->deraumere < 2 || inv->sibur < 2
+        || inv->mendiane < 2 || inv->phiras < 2 || inv->thystame < 1)
+        return false;
     return true;
 }
